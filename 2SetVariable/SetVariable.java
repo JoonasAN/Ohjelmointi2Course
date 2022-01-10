1c6f859f8f7608e5b2bd19a270ddb401a6a34051
JoonasAN <joonasan95@gmail.com>
2022-01-10 16:50:41 +0200

    closed input form 2.

--- 2SetVariable/SetVariable.java
+++ 2SetVariable/SetVariable.java
@@ -2,14 +2,16 @@ import java.util.Scanner;
 
 class SetVariable {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); // scan input from keyboard
         System.out.print("Enter an integer: ");
-        int posNum = input.nextInt();
+        int posNum = input.nextInt();   // set input into variable
         if (posNum >= 0) {
             System.out.printf("The given number is %d ", posNum);
         }
         else {
             System.out.print("The given number is not a positive integer.");
         }
+        input.close(); // close input
+
     }
 }
