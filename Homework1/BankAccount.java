237bd7d9407acb51eca0d2c286a4bafdd65afd7b
JoonasAN <joonasan95@gmail.com>
2022-01-14 17:35:13 +0200

    Homework 1 almost done

--- Homework1/BankAccount.java
+++ Homework1/BankAccount.java
@@ -6,20 +6,49 @@ public class BankAccount {
     private double balance = 0.0;
 
     // Constructor for bank account
-    public BankAccount(String name, String accId, double balanceIn){
+    public BankAccount(String name, String accId, double balanceIn) {
         this.accountOwner = name;
         this.accountNum = accId;
         this.balance = balanceIn;
     }
 
-    public void getAccount() {
-        System.out.println(accountOwner);
-        System.out.println(accountNum);
-        System.out.println(balance);
+    public String getAccountNum() {
+        //System.out.println("Account Number: " + accountNum);
+        return this.accountNum;
+    }
+    public String getAccountOwner() {
+        //System.out.println("Owner: " + accountOwner); 
+        return this.accountOwner;
+    }
+    public Double getAccountBalance() {
+        //System.out.println("Balance: " + balance);
+        return this.balance;
     }
 
-    
+    // deposits specified amount of balance to account
+    public void deposit(String amountStr) {
+        Double amount = Double.parseDouble(amountStr);
+        if (amount > 0) {
+            this.balance += amount;
+        } else if (amount < 0) {
+            System.out.println("Can't deposit negative value!");
+        } else if (amount == 0) {
+        } else {
+            System.out.println("Something went wrong, try again.");
+        }
+    }
 
-
-
+    // withdraw specified amount from account balance
+    public void withdraw(String amountStr) {
+        Double amount = Double.parseDouble(amountStr);
+        if (amount > 0 && amount <= this.balance) {
+            this.balance -= amount;
+        } else if (amount < 0) {
+            System.out.println("Can't withdraw negative value!");
+        } else if (amount > this.balance) {
+            System.out.println("Not enough balance!");
+        } else {
+            System.out.println("Something went wrong, try again.");
+        }
+    }
 }
--- Homework1/BankAccountMain.java
+++ Homework1/BankAccountMain.java
@@ -1,27 +1,134 @@
 package homework1;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 //import homework1.BankAccount.*;
 
 
 public class BankAccountMain {
     public static void main(String[] args) {
+        boolean quit = false;
+        boolean back = false;
+        String amount = null;
         Scanner input = new Scanner(System.in);
-
-        System.out.print("Full name: ");
-        String name = input.nextLine();
-
-        System.out.print("Account Number: ");
-        String accId = input.nextLine();
-
-        System.out.print("Balance: ");
-        double balance = input.nextDouble();
-
-        BankAccount acc = new BankAccount(name, accId, balance);
+        Scanner input1 = new Scanner(System.in);
+        Scanner input2 = new Scanner(System.in);
+        List<BankAccount> accounts = new ArrayList<>();
+        BankAccount acc;
         
-        acc.getAccount();
+        do {
+            // Print interface
+            System.out.println("\n\n                                    *** Welcome to ATM ***");
+            System.out.println("*** Choose action by giving desired character as an input ***");
+            System.out.println("\n  1. Make Account \n  2. Login \n  q: Quit ");
 
-        input.close();
+            switch (input1.nextLine()) {
+                // Make account
+                case "1": 
+                    System.out.print("Full name: ");
+                    String name = input1.nextLine();
+
+                    System.out.print("Account Number: ");
+                    String accId = input1.nextLine();
+
+                    System.out.print("Balance: ");
+                    double balance = input1.nextDouble();
+
+                    // make object account and add it to list
+                    accounts.add(new BankAccount(name, accId, balance));
+                    //BankAccount acc = new BankAccount(name, accId, balance);
+                    break;
+
+                // Login
+                case "2":
+                    //System.out.println("\n\n        *** Login ***\n\n");
+                    //System.out.print("Give Your Account Number: ");
+                    //String checkAcc = input.nextLine();
+
+                    System.out.println("\n\n                            *** Login ***\n\n");
+                    System.out.print("Give Your Account Number: ");
+                    String checkAcc = input2.nextLine();
+
+                    // iterate through list of accounts anc check
+                    for (int i = 0; i < accounts.size(); i++) {
+                        // choose account from list
+                        acc = accounts.get(i);
+
+                        System.out.println(checkAcc + " = " + acc.getAccountNum());
+                        /*
+                        if (checkAcc == acc.getAccountNum()) {
+                            do {
+                                try {
+                                    System.out.println("\n  1. Deposit");
+                                    System.out.println("    2. Withdraw");
+                                    System.out.println("    3. Show Account and Balance");
+                                    System.out.println("    b: Back");
+                                     
+                                    switch (input.nextLine()) {
+        
+                                        // Deposit
+                                        case "1":
+                                            System.out.print("Amount to Deposit: ");
+                                            try {
+                                                amount = input.nextLine();
+                                                acc.deposit(amount);
+                                            } catch (Exception e) {
+                                                System.out.println("Coudn't Deposit that, try again.");
+                                            }
+                                            break;
+        
+                                        // Withdraw
+                                        case "2":
+                                            System.out.print("Amount to Withdraw: ");
+                                            try {
+                                                amount = input.nextLine();
+                                                acc.withdraw(amount);
+                                            } catch (Exception e) {
+                                                System.out.println("Coudn't Withdraw that, try again.");
+                                            }
+                                            break;
+        
+                                        // Print Account
+                                        case "3":
+                                            System.out.println("Account Number: " + acc.getAccountNum());
+                                            System.out.println("Owner: " + acc.getAccountOwner());
+                                            System.out.println("Balance: " + acc.getAccountBalance());
+                                            break;
+        
+                                        // Quit
+                                        case "q":
+                                            quit = true;
+                                            break;
+                                        default:
+                                            System.out.println("Wrong input try Again.");
+                                            break;
+                                    }
+        
+                                } catch (Exception e) {
+                                    System.out.println("Exception thrown: " + e);
+                                }
+                            } while (quit == false);
+                            input.close();
+                            break;
+
+                        }else if (checkAcc == "b"){
+                            break;
+                        }else{
+                            System.out.println("Account Number Doesn't Exist!");
+                        }*/
+                    }
+                    input2.close();
+                    break;
+
+                case "q":
+                    quit = true;
+                    break;
+                default:
+                    System.out.println("Wrong input try Again.");
+                    break;
+            }
+        } while (quit == false);
+        input1.close();
     }
-    
 }
